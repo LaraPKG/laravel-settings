9d7bd3d7ddada92eaf101b99f61387b20d5110dd
Paul Dawson <paul.dawson@visualsoft.co.uk>
2020-08-07 21:29:56 +0100

    publish migrations

--- src/SettingsServiceProvider.php
+++ src/SettingsServiceProvider.php
@@ -23,6 +23,14 @@ class SettingsServiceProvider extends ServiceProvider
                 [__DIR__ . '/../config/laravel-settings.php' => config_path('laravel-settings.php')],
                 'config'
             );
+
+            if (!class_exists('CreatePackageTable')) {
+                $this->publishes([
+                    $this->migrationPath('create_setting_groups_table'),
+                    $this->migrationPath('create_settings_table'),
+                    $this->migrationPath('create_setting_values_table'),
+                ]);
+            }
         }
     }
 
@@ -32,5 +40,19 @@ class SettingsServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/laravel-settings.php', 'laravel-settings');
+    }
+
+    /**
+     * Gets a database migration path to publish
+     *
+     * @param string $migration
+     * @return array
+     */
+    protected function migrationPath(string $migration): array
+    {
+        $key = __DIR__ . '/../database/migrations/' . $migration . '.php.stub';
+        $value = database_path('migrations/' . date('Y_m_d_His') . '_' . $migration . '.php');
+
+        return [$key => $value];
     }
 }
